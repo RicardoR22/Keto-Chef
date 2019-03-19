a8a7d1b406f7ceb59fc0e038167dde6b11e741df
Ricardo Rodriguez <rr092293@gmail.com>
2019-03-19 13:41:48 -0700

    setup xcode project

---
+++ Keto-Chef/Keto-Chef/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Keto-Chef
+//
+//  Created by Ricardo Rodriguez on 3/19/19.
+//  Copyright © 2019 Ricardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
