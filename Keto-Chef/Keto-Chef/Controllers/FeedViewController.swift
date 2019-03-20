1384c67721eb529e3867181ca3969e2be051c284
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-03-20 01:00:59 -0700

    contraints and seperator for cells added

--- Keto-Chef/Keto-Chef/Controllers/FeedViewController.swift
+++ Keto-Chef/Keto-Chef/Controllers/FeedViewController.swift
@@ -16,18 +16,53 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.title = "Trending Recipes"
         collectionView?.backgroundColor = .cyan
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(RecipeCell.self, forCellWithReuseIdentifier: "cellId")
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        cell.backgroundColor = .red
+//        cell.backgroundColor = .red
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
-    }   
+        return CGSize(width: view.frame.width, height: 300)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    class RecipeCell: UICollectionViewCell {
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupViews()
+        }
+        
+        let thumbnailImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.backgroundColor = .blue
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        let seperatorView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .red
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+        
+        func setupViews() {
+            addSubview(thumbnailImageView)
+            addSubview(seperatorView)
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[v0]-16-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": seperatorView]))
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0(1)]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": seperatorView]))
+        
+        }
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
 }
 
