f718edc9ccc1c8f0e2786e153adf0bdfa89d75d0
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-03-21 00:50:32 -0700

    redesigned cell layout

--- Keto-Chef/Keto-Chef/Controllers/FeedViewController.swift
+++ Keto-Chef/Keto-Chef/Controllers/FeedViewController.swift
@@ -27,7 +27,7 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 300)
+        return CGSize(width: view.frame.width, height: 200)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
@@ -41,23 +41,64 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
         let thumbnailImageView: UIImageView = {
             let imageView = UIImageView()
             imageView.backgroundColor = .blue
-            imageView.translatesAutoresizingMaskIntoConstraints = false
             return imageView
         }()
+        
+        let profileImage: UIImageView = {
+            let imageView = UIImageView()
+            imageView.backgroundColor = .red
+            return imageView
+        }()
+        
         let seperatorView: UIView = {
             let view = UIView()
             view.backgroundColor = .black
             return view
         }()
         
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.backgroundColor = .red
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        let descriptionLabel: UILabel = {
+            let label = UILabel()
+            label.backgroundColor = .black
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
         func setupViews() {
             addSubview(thumbnailImageView)
             addSubview(seperatorView)
+            addSubview(profileImage)
+            addSubview(titleLabel)
+            addSubview(descriptionLabel)
             
-            
-                addContraintsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView)
-                addContraintsWithFormat(format: "V:|-16-[v0]-16-[v1(1)]|", views: thumbnailImageView, seperatorView)
+            //height contraints
+            addContraintsWithFormat(format: "H:|-30-[v0]-230-|", views: thumbnailImageView)
+//            addContraintsWithFormat(format: "H:|-30-[v0(44)]", views: profileImage)
+            //vertical contraints
+                addContraintsWithFormat(format: "V:|-25-[v0(150)]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView, profileImage, seperatorView)
                 addContraintsWithFormat(format: "H:|[v0]|", views: seperatorView)
+            addContraintsWithFormat(format: "H:|-200-[v0]-20-|", views: titleLabel)
+            addContraintsWithFormat(format: "V:|-25-[v0(25)]-8-|", views: titleLabel)
+            addContraintsWithFormat(format: "H:|-200-[v0]-20-|", views: descriptionLabel)
+            addContraintsWithFormat(format: "V:|-60-[v0(120)]-8-|", views: descriptionLabel)
+            
+////            top contraints
+//            addConstraints([NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .top, multiplier: 1, constant: 8)])
+////
+//            //left contraints
+//            addConstraints([NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 8)])
+//
+////            //right contraints
+//            addConstraints([NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 8)])
+//            addContraintsWithFormat(format: "V:[v0(20)]", views: titleLabel)
+//            addContraintsWithFormat(format: "H:|[v0]|", views: titleLabel)
+//            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0(1)]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": seperatorView]))
         
         }
         required init?(coder aDecoder: NSCoder) {
@@ -73,7 +114,6 @@ extension UIView {
                 let key = "v\(index)"
                 view.translatesAutoresizingMaskIntoConstraints = false
                 viewsDictionary[key] = view
-                
             }
             addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
             
